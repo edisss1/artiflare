0b2a8f51bc49d4f7d81ab81798f7cb12203d1c44
Sergey <kkotovic14@gmail.com>
2024-11-11 18:13:47 +0700

    update: added links to settings page

--- src/components/molecules/SettingsPanel.tsx
+++ src/components/molecules/SettingsPanel.tsx
@@ -1,4 +1,27 @@
+import { NavLink } from "react-router-dom";
+
 const SettingsPanel = () => {
-  return <div className="">SettingsPanel</div>
-}
-export default SettingsPanel
+  return (
+    <div className="flex flex-col bg-primary w-fit p-4 rounded-sm">
+      <NavLink className={"mb-4"} to={""}>
+        Profile settings
+      </NavLink>
+      <div className={"flex flex-col gap-6"}>
+        <div>
+          <h2 className={"font-medium"}>Account</h2>
+          <div>
+            <NavLink to={""}>Team profile</NavLink>
+          </div>
+        </div>
+
+        <div>
+          <h2 className={"font-medium"}>User management</h2>
+          <div>
+            <NavLink to={""}>Team members</NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SettingsPanel;

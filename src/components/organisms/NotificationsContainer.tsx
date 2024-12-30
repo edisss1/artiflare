8e9fcfe7aca990a34c9348eaf485a9d2c7a25a3a
Sergey Sudakov <kkotovic14@gmail.com>
2024-12-30 16:53:41 +0700

    update: little ui improvements

--- src/components/organisms/NotificationsContainer.tsx
+++ src/components/organisms/NotificationsContainer.tsx
@@ -1,30 +1,40 @@
-import {NotificationType} from "../../types/NotificationType.ts"
+import { NotificationType } from "../../types/NotificationType.ts"
 import NotificationsContainerHeader from "../atoms/NotificationsContainerHeader.tsx"
 import Notification from "../atoms/Notification.tsx"
 
-
 interface NotificationsProps {
-	isContainerOpened: boolean
-	closeContainer: () => void
-	notifications: NotificationType[]
+    isContainerOpened: boolean
+    closeContainer: () => void
+    notifications: NotificationType[]
 }
 
-
-
-const NotificationsContainer = ({isContainerOpened, closeContainer, notifications}: NotificationsProps) => {
-
-
-	return (
-		<aside
-			className={`bg-primary p-4 dark:bg-primary-dark fixed h-screen w-full drop-shadow-2xl max-w-[300px] z-10 right-0 top-0 ${!isContainerOpened ? "translate-x-full" : "translate-x-0"} transition-all duration-150 ease-in `}>
-			<NotificationsContainerHeader onClick={closeContainer}/>
-			<div className={"mt-4"}>
-				{notifications.length === 0 && <p className={"text-center text-balance mt-8"}>You don’t have any notifications yet</p>}
-				{notifications.map((notification) => (
-					<Notification {...notification} />
-				))}
-			</div>
-		</aside>
-	)
+const NotificationsContainer = ({
+    isContainerOpened,
+    closeContainer,
+    notifications
+}: NotificationsProps) => {
+    return (
+        <aside
+            className={`bg-primary p-4 dark:bg-primary-dark fixed h-screen w-full drop-shadow-2xl max-w-[300px] z-10 right-0 top-0 ${
+                !isContainerOpened ? "translate-x-full" : "translate-x-0"
+            } transition-all duration-150 ease-in `}
+        >
+            <NotificationsContainerHeader onClick={closeContainer} />
+            <div className={"mt-4"}>
+                {notifications.length === 0 && (
+                    <p
+                        className={
+                            "text-center text-balance text-sm mt-8 opacity-70"
+                        }
+                    >
+                        You don’t have any notifications yet
+                    </p>
+                )}
+                {notifications.map((notification) => (
+                    <Notification notification={notification} />
+                ))}
+            </div>
+        </aside>
+    )
 }
 export default NotificationsContainer

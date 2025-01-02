0102d916e828a316744163106673c58829443c03
Sergey <kkotovic14@gmail.com>
2025-01-03 04:56:37 +0700

    fix: some shapes weren't displayed properly

--- src/hooks/useShapes.tsx
+++ src/hooks/useShapes.tsx
@@ -30,23 +30,23 @@ export function useShapes(canvas: Canvas | null) {
     }
     const addDecision = (x: number, y: number, stroke: string) => {
         if (canvas) {
-            const diamond = new Polygon(
-                [
-                    { x: -50, y: 200 / 2 },
-                    { x: 200 / 2, y: 50 },
-                    { x: 250, y: 200 / 2 },
-                    { x: 200 / 2, y: 150 }
-                ],
+            const decision = new Path(
+                "M21.635 0.382C22.145 0.145 22.855 0.145 23.365 0.382L44.365 11.382C44.875 11.619 44.875 12.381 44.365 12.618L23.365 23.618C22.855 23.855 22.145 23.855 21.635 23.618L0.635 12.618C0.125 12.381 0.125 11.619 0.635 11.382L21.635 0.382Z",
                 {
+                    stroke,
+                    strokeWidth: 1,
+                    fill: "transparent",
+                    width: 200,
+                    height: 100,
                     left: x,
-                    top: y,
-                    fill: null,
-                    stroke: stroke,
-                    strokeWidth: 4,
-                    selectable: true
+                    top: y
                 }
             )
-            canvas?.add(diamond)
+
+            decision.scaleX = 7
+            decision.scaleY = 3.5
+
+            canvas?.add(decision)
         }
     }
 
@@ -69,108 +69,94 @@ export function useShapes(canvas: Canvas | null) {
     }
 
     const addPredefinedProcess = (x: number, y: number, stroke: string) => {
-        const predefinedProcess = new Rect({
-            left: x,
-            top: y,
-            width: 200,
-            height: 100,
-            fill: null,
-            stroke,
-            strokeWidth: 4
-        })
-        const firstLine = new Line([x, y + 100 / 4, x + 200, y + 100 / 4], {
-            stroke: stroke,
-            strokeWidth: 4
-        })
-        const secondLine = new Line(
-            [x, y + (3 * 100) / 4, x + 200, y + (3 * 100) / 4],
+        const firstPath = new Path(
+            "M2 2H43C44.1046 2 45 2.89543 45 4V20C45 21.1046 44.1046 22 43 22H2C0.89543 22 0 21.1046 0 20V4C0 2.89543 0.89543 2 2 2Z",
             {
-                stroke,
-                strokeWidth: 4
+                stroke: stroke,
+                strokeWidth: 1,
+                fill: "transparent"
             }
         )
-        const group = new Group([predefinedProcess, firstLine, secondLine], {
-            top: y,
-            left: x
+
+        const secondPath = new Path("M2 2V22", {
+            stroke: stroke,
+            strokeWidth: 1,
+            fill: "transparent"
         })
-        canvas?.add(group)
+
+        const thirdPath = new Path("M43 2V22", {
+            stroke: stroke,
+            strokeWidth: 1,
+            fill: "transparent"
+        })
+
+        const predefinedProcess = new Group(
+            [firstPath, secondPath, thirdPath],
+            {
+                left: x,
+                top: y,
+                width: 200,
+                height: 100
+            }
+        )
+
+        predefinedProcess.scaleX = 7
+        predefinedProcess.scaleY = 5
+
+        canvas?.add(predefinedProcess)
     }
 
     const addDocument = (x: number, y: number, stroke: string) => {
-        const path = new Path(
-            `M 0,0 L ${200},0 L ${200},${100} Q ${200 / 2},${
-                100 + 20
-            } 0,${100} Z`,
+        const document = new Path(
+            "M2 2H402V185.6C202 110.562 202 262.762 2 172.293V2Z",
             {
                 left: x,
                 top: y,
                 fill: "transparent",
-                stroke,
+                stroke: stroke,
                 strokeWidth: 4
             }
         )
-        canvas?.add(path)
+
+        canvas?.add(document)
     }
 
     const addMultipleDocuments = (x: number, y: number, stroke: string) => {
-        const doc1 = new Rect({
+        const firstDoc = new Path(
+            "M30 2H430V185.599C230 110.566 230 262.759 30 172.295V2Z",
+            {
+                stroke: stroke,
+                strokeWidth: 4,
+                fill: ""
+            }
+        )
+        const seconDoc = new Path(
+            "M18.5 22.5H418.5V206.099C218.5 131.066 218.5 283.259 18.5 192.795V22.5Z",
+            {
+                stroke: stroke,
+                strokeWidth: 4,
+                fill: ""
+            }
+        )
+
+        const thirdDoc = new Path(
+            "M2 43H402V226.599C202 151.566 202 303.759 2 213.295V43Z",
+            {
+                stroke: stroke,
+                strokeWidth: 4,
+                fill: ""
+            }
+        )
+
+        const documents = new Group([firstDoc, seconDoc, thirdDoc], {
+            top: y,
             left: x,
-            top: y,
-            width: 200,
-            height: 100,
-            fill: null,
-            stroke,
-            strokeWidth: 4
-        })
-        const doc2 = new Rect({
-            left: x - 10,
-            top: y + 10,
-            width: 200,
-            height: 100,
-            fill: null,
-            stroke,
-            strokeWidth: 4
-        })
-        const doc3 = new Rect({
-            left: x - 20,
-            top: y + 20,
-            width: 200,
-            height: 100,
-            fill: null,
-            stroke,
+            fill: "transparent",
+            stroke: stroke,
             strokeWidth: 4
         })
 
-        // const firstPath = new Path(
-        //     `M 0,0 L ${200},0 L ${200},${100} Q ${200 / 2},${
-        //         100 + 20
-        //     } 0,${100} Z`,
-        //     {
-        //         left: x,
-        //         top: y,
-        //         fill: "transparent",
-        //         stroke,
-        //         strokeWidth: 4
-        //     }
-        // )
-        // const secondPath = new Path(
-        //     `M 0,0 L ${200 + 10},0 L ${200 + 10},${100 + 10} Q ${
-        //         200 / 2
-        //     },${100 + 20 + 10} 0,${100 + 10} Z`,
-        //     {
-        //         left: x,
-        //         top: y,
-        //         fill: "transparent",
-        //         stroke,
-        //         strokeWidth: 4
-        //     }
-        // )
-
-        const group = new Group([doc1, doc2, doc3], {
-            top: y,
-            left: x
-        })
-        canvas?.add(group)
+        canvas?.add(documents)
     }
 
     const addInputOutput = (x: number, y: number, stroke: string) => {
@@ -193,99 +179,127 @@ export function useShapes(canvas: Canvas | null) {
     }
 
     const addPreparation = (x: number, y: number, stroke: string) => {
-        const preparation = new Polygon(
-            [
-                { x: 200 / 4, y: 0 },
-                { x: (3 * 200) / 4, y: 0 },
-                { x: 200, y: 100 },
-                { x: 0, y: 100 }
-            ],
+        const preparation = new Path(
+            "M55.3333 202L2 102L55.3333 2H348.667L402 102L348.667 202H55.3333Z",
             {
-                left: x,
-                top: y,
-                fill: "transparent",
                 stroke,
-                strokeWidth: 4
+                strokeWidth: 4,
+                top: y,
+                left: x,
+                fill: "transparent"
             }
         )
         canvas?.add(preparation)
     }
 
     const addDatabase = (x: number, y: number, stroke: string) => {
-        const database = new Ellipse({
-            left: x,
-            top: y,
-            width: 200,
-            height: 100,
-            rx: 200 / 2,
-            ry: 100 / 2,
-            fill: null,
-            stroke,
-            strokeWidth: 4
-        })
+        const firstPath = new Path(
+            "M2 27C2 39.5 84.5 52 189.5 52C294.5 52 377 39.5 377 27V177C377 189.5 294.5 202 189.5 202C84.5 202 2 189.5 2 177V27Z",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: "transparent"
+            }
+        )
+        const secondPath = new Path(
+            "M377 26.5C377 38.75 294.5 51 189.5 51C84.5 51 2 38.75 2 26.5C2 14.25 84.5 2 189.5 2C294.5 2 377 14.25 377 26.5Z",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: "transparent"
+            }
+        )
+
+        const thirdPath = new Path(
+            "M2 39C2 52 84.5 65 189.5 65C294.5 65 377 52 377 39",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: "transparent"
+            }
+        )
+        const fourthPath = new Path(
+            "M2 51C2 64.5 84.5 78 189.5 78C294.5 78 377 64.5 377 51",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: "transparent"
+            }
+        )
+
+        const database = new Group(
+            [firstPath, secondPath, thirdPath, fourthPath],
+            {
+                left: x,
+                top: y
+            }
+        )
 
         canvas?.add(database)
     }
 
     const addDirectData = (x: number, y: number, stroke: string) => {
-        const directData = new Polygon(
-            [
-                { x: 0, y: 0 },
-                { x: 200, y: 0 },
-                { x: 200, y: 100 },
-                { x: 0, y: 100 }
-            ],
+        const firstPath = new Path(
+            "M34.087 202C18.7141 202 2 159.14 2 102C2 44.86 18.7141 2 34.087 2H371C354.957 2 338.913 44.86 338.913 102C338.913 159.14 354.957 202 371 202H34.087Z",
             {
-                left: x,
-                top: y,
-                fill: "transparent",
                 stroke,
-                strokeWidth: 4
+                strokeWidth: 4,
+                fill: ""
             }
         )
+
+        const secondPath = new Path(
+            "M370 202C354 202 338 159.14 338 102C338 44.86 354 2 370 2C386 2 402 44.86 402 102C402 159.14 386 202 370 202Z",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: ""
+            }
+        )
+
+        const directData = new Group([firstPath, secondPath], {
+            left: x,
+            top: y
+        })
         canvas?.add(directData)
     }
 
     const addInternalStorage = (x: number, y: number, stroke: string) => {
-        const rect = new Rect({
-            left: x,
-            top: y,
-            width: 200,
-            height: 100,
-            fill: "transparent",
+        const firstPath = new Path("M2 2H402V202H2V2Z", {
             stroke,
-            strokeWidth: 4
-        })
-        const line = new Line([x + 200 / 4, y, x + 200 / 4, y + 100], {
-            stroke,
-            strokeWidth: 4
+            strokeWidth: 4,
+            fill: "transparent"
         })
 
-        const group = new Group([rect, line], {
+        const secondPath = new Path("M55.3333 2V202", {
+            stroke,
+            strokeWidth: 4,
+            fill: "transparent"
+        })
+
+        const thirdPath = new Path("M2 55.3333H402", {
+            stroke,
+            strokeWidth: 4,
+            fill: "transparent"
+        })
+
+        const internalStorage = new Group([firstPath, secondPath, thirdPath], {
             top: y,
             left: x
         })
 
-        canvas?.add(group)
+        canvas?.add(internalStorage)
     }
 
     const addManualLoop = (x: number, y: number, stroke: string) => {
-        const polygon = new Polygon(
-            [
-                { x: 0, y: 0 },
-                { x: 200, y: 0 },
-                { x: 200 / 2, y: 100 },
-                { x: 0, y: 100 }
-            ],
-            {
-                left: x,
-                top: y,
-                fill: "transparent",
-                stroke,
-                strokeWidth: 4
-            }
-        )
-        canvas?.add(polygon)
+        const manualLoop = new Path("M2 2L55.3333 202H348.667L402 2H2Z", {
+            stroke,
+            strokeWidth: 4,
+            fill: "",
+            top: y,
+            left: x
+        })
+        canvas?.add(manualLoop)
     }
 
     const addDelay = (x: number, y: number, stroke: string) => {
@@ -305,17 +319,20 @@ export function useShapes(canvas: Canvas | null) {
     }
 
     const addStoredData = (x: number, y: number, stroke: string) => {
-        const path = new Path(
-            `M 0,0 L ${200},0 L ${200 - 20},${100} L -20,${100} Z`,
+        const storedData = new Path(
+            "M103.333 202C50 202 2 159.14 2 102C2 44.86 50 2 103.333 2H402C339.778 68.6667 339.778 135.333 402 202H103.333Z",
             {
                 left: x,
                 top: y,
                 fill: "transparent",
-                stroke,
-                strokeWidth: 4
+                stroke: stroke,
+                strokeWidth: 4,
+                width: 200,
+                height: 100
             }
         )
-        canvas?.add(path)
+
+        canvas?.add(storedData)
     }
 
     const addMerge = (x: number, y: number, stroke: string) => {
@@ -349,42 +366,66 @@ export function useShapes(canvas: Canvas | null) {
     }
 
     const addOr = (x: number, y: number, stroke: string) => {
-        const or = new Polygon(
-            [
-                { x: 200 / 2, y: 0 },
-                { x: 200, y: 100 / 2 },
-                { x: 200 / 2, y: 100 },
-                { x: 0, y: 100 / 2 }
-            ],
+        const firstPath = new Path(
+            "M102 202C157.228 202 202 157.228 202 102C202 46.7715 157.228 2 102 2C46.7715 2 2 46.7715 2 102C2 157.228 46.7715 202 102 202Z",
             {
-                left: x,
-                top: y,
-                fill: "transparent",
                 stroke,
-                strokeWidth: 4
+                strokeWidth: 4,
+                fill: "transparent"
             }
         )
+
+        const secondPath = new Path("M102 2V202", {
+            stroke,
+            strokeWidth: 4,
+            fill: "transparent"
+        })
+        const thirdPath = new Path("M202 102H2", {
+            stroke,
+            strokeWidth: 4,
+            fill: "transparent"
+        })
+
+        const or = new Group([firstPath, secondPath, thirdPath], {
+            top: y,
+            left: x
+        })
         canvas?.add(or)
     }
 
     const addSummingJunction = (x: number, y: number, stroke: string) => {
-        const circle = new Circle({
-            left: x,
-            top: y,
-            radius: 75,
-            fill: "transparent",
+        const firstPath = new Path(
+            "M102 202C157.228 202 202 157.228 202 102C202 46.7715 157.228 2 102 2C46.7715 2 2 46.7715 2 102C2 157.228 46.7715 202 102 202Z",
+            {
+                stroke,
+                strokeWidth: 4,
+                fill: "transparent"
+            }
+        )
+
+        const secondPath = new Path("M31.5 31.5L171.5 171.5", {
             stroke,
             strokeWidth: 4,
-            selectable: true
+            fill: "transparent"
         })
-        canvas?.add(circle)
+
+        const thirdPath = new Path("M175.5 35.5L35.5 175.5", {
+            stroke,
+            strokeWidth: 4,
+            fill: "transparent"
+        })
+
+        const summingJunction = new Group([firstPath, secondPath, thirdPath], {
+            left: x,
+            top: y
+        })
+
+        canvas?.add(summingJunction)
     }
 
     const addDisplay = (x: number, y: number, stroke: string) => {
-        const path = new Path(
-            `M 0,0 L ${200 - 20},0 A 20,20 0 0,1 ${200},20 L ${200},${
-                100 - 20
-            } A 20,20 0 0,1 ${200 - 20},${100} L 0,${200} Z`,
+        const display = new Path(
+            "M82 202L2 102L82 2H308.669C362 2 402 44.86 402 102C402 159.14 362 202 308.669 202H82Z",
             {
                 left: x,
                 top: y,
@@ -393,27 +434,22 @@ export function useShapes(canvas: Canvas | null) {
                 strokeWidth: 4
             }
         )
-        canvas?.add(path)
+        canvas?.add(display)
     }
 
     const addOffPageConnector = (x: number, y: number, stroke: string) => {
-        const polygon = new Polygon(
-            [
-                { x: 0, y: 0 }, // Top left
-                { x: 200 / 2, y: 0 },
-                { x: 200, y: 50 },
-                { x: 200 / 2, y: 100 },
-                { x: 100, y: 100 / 2 }
-            ],
+        const offPageConnector = new Path(
+            "M1.99999 2.00001L2 102L102 202L202 102L202 2L1.99999 2.00001Z",
             {
-                left: x,
-                top: y,
+                stroke: stroke,
+                strokeWidth: 4,
                 fill: "transparent",
-                stroke,
-                strokeWidth: 4
+                left: x,
+                top: y
             }
         )
-        canvas?.add(polygon)
+
+        canvas?.add(offPageConnector)
     }
 
     const addRectangle = (

970f114153e0a4a7395ea51294aedf47b3ca8f1d
Sergey Sudakov <kkotovic14@gmail.com>
2025-02-16 14:12:22 +0700

    fix: overflow was hidden in settings

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,7 @@
 import DashboardSidebar from "../components/organisms/DashboardSidebar"
 import DashboardHome from "../components/organisms/DashboardHome.tsx"
-import { useEffect } from "react"
 
 const Dashboard = () => {
-    useEffect(() => {
-        document.body.style.overflow = "hidden"
-    }, [])
-
     return (
         <main className="dashboard">
             <DashboardSidebar />

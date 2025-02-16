0b3de3aeaaac0f22a10f4c17064e4a5f739475c1
Sergey Sudakov <kkotovic14@gmail.com>
2025-02-16 13:59:42 +0700

    fix: dashboard overflow

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,12 @@
 import DashboardSidebar from "../components/organisms/DashboardSidebar"
 import DashboardHome from "../components/organisms/DashboardHome.tsx"
+import { useEffect } from "react"
 
 const Dashboard = () => {
+    useEffect(() => {
+        document.body.style.overflow = "hidden"
+    }, [])
+
     return (
         <main className="dashboard">
             <DashboardSidebar />

1bd0220f33785b90a01a9e469a009a41d1296e1f
Sergey Sudakov <kkotovic14@gmail.com>
2025-02-21 16:36:52 +0700

    fix: sign up button was always disabled

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -163,9 +163,7 @@ const Form = ({
                 )}
                 {isSignUp ? (
                     <Button
-                        disabled={
-                            !passwordMatch || !email || isSignUp || !isAgreed
-                        }
+                        disabled={!passwordMatch || !email || !isAgreed}
                         className="my-6 w-full bg-secondary py-4 rounded-lg  enabled:hover:opacity-65 transition-opacity duration-150 disabled:opacity-50 disabled:cursor-not-allowed"
                     >
                         {children}
